d0b08fb440d6139721d17283fe32a0fb139334af
Srinivasan Raghunathan <sraghunathan@MacBook-Pro-79.home>
2022-09-29 18:53:10 -0500

    minor

--- trigger_fisher.py
+++ trigger_fisher.py
@@ -2,19 +2,23 @@ import numpy as np, os, sys
 
 which_spectra_arr = ['delensed_scalar', 'unlensed_total', 'total']
 noise_level_arr = np.arange(1., 11., 1.)
+make_batch_scripts = False
 
 for which_spectra in which_spectra_arr:
     for noise_level in noise_level_arr:
-        template_fname='batch_jobs/template_slurm.sh'
-        opfname = 'batch_jobs/fisher_%s_noise%s.sh' %(which_spectra, noise_level)
-        opf=open(opfname, 'w')
-        template=open(template_fname, 'r')
-        for line in template:
-            opf.writelines('%s\n' %(line.strip()))
         opline = 'python3 fisher_diff_noise.py -which_spectra %s -rms_map_T %s' %(which_spectra, noise_level)
-        opf.writelines('%s\n' %(opline))
-        opf.close()
-        cmd='sbatch %s' %(opfname)
-        os.system(cmd)
-        print('\n%s'%(cmd))
+        if make_batch_scripts:
+            template_fname='batch_jobs/template_slurm.sh'
+            opfname = 'batch_jobs/fisher_%s_noise%s.sh' %(which_spectra, noise_level)
+            opf=open(opfname, 'w')
+            template=open(template_fname, 'r')
+            for line in template:
+                opf.writelines('%s\n' %(line.strip()))
+            opf.writelines('%s\n' %(opline))
+            opf.close()
+            cmd='sbatch %s' %(opfname)
+            os.system(cmd)
+            print('\n%s'%(cmd))
+        else:
+            print(opline)
 sys.exit()
