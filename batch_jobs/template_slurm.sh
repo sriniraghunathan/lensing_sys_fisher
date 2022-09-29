7c6ad64a331f6d94b1e6275146bc6012d767a187
Srinivasan Raghunathan <sraghunathan@eduroamprvnat-172-16-32-213.near.illinois.edu>
2022-09-29 16:50:17 -0500

    adding batch scripts

--- batch_jobs/template_slurm.sh
+++ batch_jobs/template_slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1
 ##SBATCH --mem 10000
 #SBATCH --time=4:00:00
-#SBATCH --output=batch_jobs/cross_ilc/job.o%j
+#SBATCH --output=batch_jobs/job.o%j
 ##export SHELL=bash
 ##eval `/cvmfs/spt.opensciencegrid.org/py3-v2/setup.sh`
 #cd /u/home/s/srinirag/projects_caps/spt3g_software/build
